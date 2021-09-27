2f477d0ebd5d56561ce0cfbcfae2ce7c2c1d51b0
Tanvi Kshatriya <punamtanvi.kshatriya18@vit.edu>
2021-09-27 16:21:15 +0530

    upload files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [quote, setQuote] = useState('');
+
+    const getQuotes = () => {
+        fetch("http://api.quotable.io/random")
+            .then((res) => res.json())
+            .then(quote => {
+                setQuote(quote.content);
+            })
+    }
+
+    useEffect( () => {
+        getQuotes();
+    }, []);
+
+    return (
+        <div className="app">
+          <div className="card">
+              <h1 className="heading">
+                {quote}
+              </h1>
+              <button className="button" onClick={getQuotes}>
+                <span>GENERATE ANOTHER QUOTE!</span>
+              </button>
+          </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
